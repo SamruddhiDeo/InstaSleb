ce6371774c632e680ba8f040c32a7557a27388fb
SamruddhiDeo <samruddhideo3@gmail.com>
2024-04-02 10:33:33 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/instasleb/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.instasleb;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}

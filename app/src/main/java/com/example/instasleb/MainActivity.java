49b265d0621674d074b9cc7f477ecded271ff0ec
SamruddhiDeo <samruddhideo3@gmail.com>
2024-04-03 18:58:00 +0530

    Final Commit

--- app/src/main/java/com/example/instasleb/MainActivity.java
+++ app/src/main/java/com/example/instasleb/MainActivity.java
@@ -1,19 +1,35 @@
 package com.example.instasleb;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
-import androidx.core.content.ContextCompat;
-
-import android.graphics.drawable.Drawable;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.util.DisplayMetrics;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        final Configuration configuration = newBase.getResources().getConfiguration();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+        {
+            final DisplayMetrics displayMetrics = newBase.getResources().getDisplayMetrics();
+            if (displayMetrics.densityDpi != DisplayMetrics.DENSITY_DEVICE_STABLE)
+            {
+                // Current density is different from Default Density. Override it
+                configuration.densityDpi = DisplayMetrics.DENSITY_DEVICE_STABLE;
+            }
+        }
+        configuration.fontScale = 1.0f;
+        Context newContext = newBase.createConfigurationContext(configuration);
+        super.attachBaseContext(newContext);
     }
 }
